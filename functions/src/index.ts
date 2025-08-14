4807aca680c209a87e4c07abfbaaceeb2d824f41
M Bhjd <itsmb128@gmail.com>
2025-08-14 09:49:27 +0000

    Error: The file "/vercel/path0/.next/routes-manifest.json" couldn't be f

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,7 +3,7 @@ import next from 'next';
 import path from 'path';
 
 const isDev = process.env.NODE_ENV !== 'production';
-const nextAppDir = path.join(__dirname, '.');
+const nextAppDir = path.join(__dirname, '..');
 
 const nextApp = next({
   dev: isDev,
